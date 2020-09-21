84ecce68b096f3875d07155101d9f796311a10c0
jaehyeon48 <imjaehyeon48@gmail.com>
2020-09-21 17:30:47 +0900

    Modified main page's url and added header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './components/Main';
+import './app.css';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Main} exact={true} />
+        <Route path="/dsvisualizer" component={Main} exact={true} />
       </Switch>
     </Router>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Main = () => {
   return (
     <div className="main-page">
+      <h1 className="main-page-header">
+        Data Structures Visualizer
+      </h1>
       <div></div>
     </div>
   );
