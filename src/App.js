b93103ee4aa8c3a8ea13505524fec7fd50f4f1ca
jaehyeon48 <imjaehyeon48@gmail.com>
2020-09-21 20:01:15 +0900

    reorganized doubly linked list's file structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './components/Main';
-import DoublyLinkedList from './components/DoublyLinkedList';
+import DoublyLinkedList from './components/doublyLinkedList/DoublyLinkedList';
 import './app.css';
 
 function App() {
