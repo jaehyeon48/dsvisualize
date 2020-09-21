55d3d4e319c881409065320f2dea3b9aff26743c
jaehyeon48 <imjaehyeon48@gmail.com>
2020-09-21 18:24:49 +0900

    Add main page style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import './main.css';
+
 const Main = () => {
   return (
     <div className="main-page">
       <h1 className="main-page-header">
         Data Structures Visualizer
       </h1>
-      <div></div>
+      <div className="select-data-structures">
+        <div className="data-structures-item sll">Singly Linked List</div>
+        <div className="data-structures-item dll">Doubly Linked List</div>
+        <div className="data-structures-item stack">Stack</div>
+        <div className="data-structures-item queue">Queue</div>
+        <div className="data-structures-item bst">Binary Search Tree</div>
+      </div>
     </div>
   );
 }
