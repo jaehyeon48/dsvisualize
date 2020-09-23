a55179a36eb1ef7a90537347c3e3b77c03fcd66a
jaehyeon48 <imjaehyeon48@gmail.com>
2020-09-23 17:58:41 +0900

    added basename for SPA of github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './app.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/dsvisualizer">
       <Switch>
         <Route path="/dsvisualizer" component={Main} exact={true} />
         <Route path="/dll" component={DoublyLinkedList} exact={true} />
